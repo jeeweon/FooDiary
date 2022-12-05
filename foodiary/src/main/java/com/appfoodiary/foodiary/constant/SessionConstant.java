85c9362204ed5f76869e34cb2bc43dad7ea72390
ra7280 <lhr7280@gmail.com>
2022-12-05 20:08:12 +0900

    SessionConstant : loginNo, loginNick

---
+++ foodiary/src/main/java/com/appfoodiary/foodiary/constant/SessionConstant.java
@@ -0,0 +1,9 @@
+package com.appfoodiary.foodiary.constant;
+
+public interface SessionConstant {
+	String NO = "loginNo";
+	//"loginNo" 대신 SessionConstant.NO
+	
+	String NICK = "loginNick";
+	//"loginNick" 대신 SessionConstant.NICK
+}
