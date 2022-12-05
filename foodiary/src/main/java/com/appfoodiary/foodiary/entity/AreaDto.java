fe31687ba6beb5fb3c97cf586fba2e8125db68c0
jeeweon <jeeweon@hanmail.net>
2022-12-05 10:52:22 +0900

    AreaDto 생성

---
+++ foodiary/src/main/java/com/appfoodiary/foodiary/entity/AreaDto.java
@@ -0,0 +1,15 @@
+package com.appfoodiary.foodiary.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class AreaDto {
+	private int areaNo;
+	private String area_city;
+	private String areaDistrict;
+	private String areaNearby1;
+	private String areaNearby2;
+}
