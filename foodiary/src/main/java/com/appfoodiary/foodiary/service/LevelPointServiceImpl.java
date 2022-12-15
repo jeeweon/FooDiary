5a2643280db8fa663bbe63071d283a1e03b0ff69
miniary <oooo8912@naver.com>
2022-12-15 14:27:22 +0900

    리뷰, 좋아요 포인트 서비스화

---
+++ foodiary/src/main/java/com/appfoodiary/foodiary/service/LevelPointServiceImpl.java
@@ -0,0 +1,42 @@
+package com.appfoodiary.foodiary.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appfoodiary.foodiary.component.LevelPoint;
+import com.appfoodiary.foodiary.entity.MemDto;
+import com.appfoodiary.foodiary.repository.MemDao;
+
+@Service
+public class LevelPointServiceImpl implements LevelPointService{
+
+	@Autowired
+	private MemDao memDao;
+	
+	@Autowired
+	private LevelPoint levelPoint;
+	
+	@Override
+	public void ReviewPoint(int memNo) {
+		
+		MemDto memDto = memDao.selectOne(memNo);
+		int originPoint = memDto.getMemPoint();
+		
+		int reviewPoint = levelPoint.reviewPoint();
+		int updatePoint = originPoint + reviewPoint;
+		memDto.setMemPoint(updatePoint);
+		memDao.updateReviewPoint(memDto);
+	}
+	
+	@Override
+	public void LikePoint(int memNo) {
+		MemDto memDto = memDao.selectOne(memNo);
+		int originPoint = memDto.getMemPoint();
+		
+		int likePoint = levelPoint.likePoint();
+		int updatePoint = originPoint + likePoint;
+		memDto.setMemPoint(updatePoint);
+		memDao.updateReviewPoint(memDto);
+		
+	}
+}
