5bd1c8b45e24dd64a345e1dfd321ce3464f48421
jeeweon <jeeweon@hanmail.net>
2022-12-18 11:25:43 +0900

    프로필 설정 여부 조회, 모델에 첨부

--- foodiary/src/main/java/com/appfoodiary/foodiary/controller/HomeController.java
+++ foodiary/src/main/java/com/appfoodiary/foodiary/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.appfoodiary.foodiary.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.appfoodiary.foodiary.constant.SessionConstant;
+import com.appfoodiary.foodiary.entity.AttachDto;
+import com.appfoodiary.foodiary.repository.MemDao;
 import com.appfoodiary.foodiary.service.LevelPointService;
 
 @Controller
@@ -17,9 +22,15 @@ public class HomeController {
 	@Autowired
 	LevelPointService levelPointService;
 	
+	@Autowired
+	private MemDao memDao;
+	
 	@GetMapping("")
-	public String home(HttpSession session) {
+	public String home(HttpSession session, Model model) {
 		int memNo = (int) session.getAttribute(SessionConstant.NO);
+		//프로필 조회
+		model.addAttribute("profile",memDao.findProfile(memNo));
+		
 		//레벨 업데이트
 		levelPointService.levelUp(memNo);
 		return "home/home";
