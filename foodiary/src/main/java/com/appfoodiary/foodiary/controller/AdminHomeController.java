9bf4d0633bc1c577e2584e45506b7ef1b861c1c8
jeeweon <jeeweon@hanmail.net>
2022-12-19 22:07:45 +0900

    신고관리 기능 분리

--- foodiary/src/main/java/com/appfoodiary/foodiary/controller/AdminHomeController.java
+++ foodiary/src/main/java/com/appfoodiary/foodiary/controller/AdminHomeController.java
@@ -4,10 +4,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.appfoodiary.foodiary.constant.SessionConstant;
 import com.appfoodiary.foodiary.entity.AdminDto;
@@ -53,10 +56,5 @@ public class AdminHomeController {
 	public String logout(HttpSession session) {
 		session.removeAttribute(SessionConstant.ID);
 		return "redirect:login";
-	}
-	
-	@GetMapping("/review-blind")
-	public String reviewBlind() {
-		return "admin/review-blind";
 	}
 }
