c131d0f789c1ede98d44597384ff7115b32554df
jeeweon <jeeweon@hanmail.net>
2022-12-20 02:21:51 +0900

    블라인드 해제 기능 구현 완료(ajax)

--- foodiary/src/main/java/com/appfoodiary/foodiary/restcontroller/AdminBlindRestController.java
+++ foodiary/src/main/java/com/appfoodiary/foodiary/restcontroller/AdminBlindRestController.java
@@ -1,14 +1,12 @@
 package com.appfoodiary.foodiary.restcontroller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appfoodiary.foodiary.repository.AdminBlindDao;
-import com.appfoodiary.foodiary.vo.ReviewBlindVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,4 +17,12 @@ public class AdminBlindRestController {
 	@Autowired
 	private AdminBlindDao blindDao;
 	
+	@PostMapping("/review/clear")
+	public boolean reviewBlindClear(@RequestParam int reviewNo) {
+		if(blindDao.reviewCntReset(reviewNo) && blindDao.reviewBlindClear(reviewNo)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
