4a3b1df2b5cf4c6c8b4e69e15fe2435e5ad929f7
earloflemongrab31 <57470776+earloflemongrab31@users.noreply.github.com>
2022-12-21 18:14:16 +0900

    Update foodiary.sql
    likes / bookmark 테이블에 primary key 추가

--- database/foodiary.sql
+++ database/foodiary.sql
@@ -109,11 +109,13 @@ left outer join profile_attach P
      on M.mem_no = P.mem_no;
 
 CREATE TABLE likes ( --like가 예약어라 likes로 대체
+        likes_no number primary key,
         review_no references review(review_no) on delete cascade,
         mem_no references mem(mem_no) on delete cascade
 );
 
 CREATE TABLE bookmark (
+        bookmark_no number primary key,
         mem_no references mem(mem_no) on delete cascade,
         review_no references review(review_no) on delete cascade,
         bookmark_time date default sysdate not null
