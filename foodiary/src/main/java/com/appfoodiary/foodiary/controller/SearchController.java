da6cb8402a6841e4400932f6a2a5be47a9ea16aa
jeeweon <jeeweon@hanmail.net>
2022-12-23 02:28:53 +0900

    안 읽은 알림 개수 첨부

--- foodiary/src/main/java/com/appfoodiary/foodiary/controller/SearchController.java
+++ foodiary/src/main/java/com/appfoodiary/foodiary/controller/SearchController.java
@@ -1,19 +1,57 @@
 package com.appfoodiary.foodiary.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.appfoodiary.foodiary.constant.SessionConstant;
+import com.appfoodiary.foodiary.repository.MemDao;
+import com.appfoodiary.foodiary.repository.NotiDao;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
+	@Autowired
+	private MemDao memDao;
+	
+	@Autowired
+	private NotiDao notiDao;
+	
 	@GetMapping("/review")
-	public String searchReview() {
+	public String searchReview(HttpSession session, Model model) {
+		if((Integer) session.getAttribute(SessionConstant.NO) != null) {
+			int memNo = (Integer) session.getAttribute(SessionConstant.NO);
+			//프로필 조회
+			model.addAttribute("profile",memDao.findProfile(memNo));
+			
+			//레벨, 포인트만 조회
+			model.addAttribute("level", memDao.memLevelAndPoint(memNo));
+			
+			//알림 내역 조회
+			model.addAttribute("list", notiDao.myNotiList(memNo));
+			model.addAttribute("cnt", notiDao.noReadCnt(memNo));
+		} 
 		return "search/review-search";
 	}
 	
 	@GetMapping("/mem")
-	public String searchMem() {
+	public String searchMem(HttpSession session, Model model) {
+		if((Integer) session.getAttribute(SessionConstant.NO) != null) {
+			int memNo = (Integer) session.getAttribute(SessionConstant.NO);
+			//프로필 조회
+			model.addAttribute("profile",memDao.findProfile(memNo));
+			
+			//레벨, 포인트만 조회
+			model.addAttribute("level", memDao.memLevelAndPoint(memNo));
+			
+			//알림 내역 조회
+			model.addAttribute("list", notiDao.myNotiList(memNo));
+			model.addAttribute("cnt", notiDao.noReadCnt(memNo));
+		} 
 		return "search/mem-search";
 	}
 }
